978e421208809720dcbb680a70d1227c0a2f6105
GOWTHAM ARULMURUGA <gowthamarummurugan2003@gmail.com>
2024-07-24 18:42:40 +0530

    3rd commit

--- Final year project - car sharing/dbconnect.php
+++
@@ -1,5 +0,0 @@
-<?php
-//$connect=mysql_connect("localhost","root","");
-//mysql_select_db("driver",$connect);
-$connect=mysqli_connect("localhost","root","","driver3") or die ("connection error") ;
-?>
